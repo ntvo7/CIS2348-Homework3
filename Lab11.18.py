03533f3b07e9259cb9c2585807ab644a2459e075
ntvo7 <77943122+ntvo7@users.noreply.github.com>
2021-03-31 05:50:30 -0500

    Update Lab11.18.py

--- Lab11.18.py
+++ Lab11.18.py
@@ -5,11 +5,11 @@
 numbers = input().split()
 listNumber = []
 
-for i in numbers:
-    i = int(i)
+for i in numbers:                       #convert from string to int so that any number greater than 0 will add to the list listNumber
+    i = int(i)                    
     if i >= 0:
         listNumber.append(i)
 
-listNumber.sort()
+listNumber.sort()                           #sort from least to greatest before print out 
 for i in listNumber:
     print(i, end =' ')
