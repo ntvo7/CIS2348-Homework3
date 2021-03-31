681c7c03b978ea91b4f8c0c0ebd952e87b689193
ntvo7 <77943122+ntvo7@users.noreply.github.com>
2021-03-31 05:29:23 -0500

    Update Lab10.15.py

--- Lab10.15.py
+++ Lab10.15.py
@@ -3,16 +3,16 @@
 
 
 class Team:
-    def __init__(self,):
+    def __init__(self):                  #starting class Team definition 
         self.teamname = 'none'
         self.team_wins = 0
         self.team_losses = 0
 
-    def get_win_percentage(self):
+    def get_win_percentage(self):                 #calculate by win amount divide by the total of both win and loss
         return self.team_wins / (self.team_wins + self.team_losses)
 
 if __name__ == "__main__":
-    team = Team()
+    team = Team()                 #main 
     teamName = input()
     teamWins = int(input())
     teamLoss = int(input())
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     team.team_wins = teamWins
     team.team_losses = teamLoss
 
-    if team.get_win_percentage() > 0.5:
+    if team.get_win_percentage() > 0.5:              #0.5 = 50% if team above 50% means they wining average 
         print('Congratulations, Team', team.teamname, 'has a winning average!')
     else:
-        print('Team', team.teamname, 'has a losing average.')
+        print('Team', team.teamname, 'has a losing average.')        
