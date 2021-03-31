c1498c55d040257d3c9055b3ba6d65e9999f6fe9
ntvo7 <77943122+ntvo7@users.noreply.github.com>
2021-03-31 05:35:44 -0500

    Update Lab10.17.py

--- Lab10.17.py
+++ Lab10.17.py
@@ -2,14 +2,17 @@
 #PISD: 1673509
 
 
-class ItemToPurchase:
+class ItemToPurchase:        #Start itemtopurchase definition
     def __init__(self):
         self.item_name = 'none'
         self.item_price = 0.0
         self.item_quantity = 0
 
-    def print_item_cost(self):
+    def print_item_cost(self):             #this will print statment for items being call in main body part
         print(self.item_name + ' ' + str(self.item_quantity) + ' @ $' + str(int(self.item_price)) + ' = $' + str(int(self.item_price * self.item_quantity)))
+        
+        
+#main body part         
 if __name__ == '__main__':
     print('Item 1')
     item1 = ItemToPurchase()
@@ -23,9 +26,10 @@ if __name__ == '__main__':
     item2.item_price = float(input('Enter the item price:\n'))
     item2.item_quantity = int(input('Enter the item quantity:\n'))
 
+#after getting info from both items then proceed to calculate total Cost
     totalCost = (item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity)
 
-    print('\nTOTAL COST')
+    print('\nTOTAL COST')          #complete program by output statement for items after retrieving info 
     item1.print_item_cost()
     item2.print_item_cost()
     print('\nTotal: $' + str(int(totalCost)))
